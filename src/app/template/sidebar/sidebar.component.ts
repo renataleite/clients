c9ecb6a29e7626470ac44a4cf25b0dc24e35249d
Renata <renataleitesg@gmail.com>
2021-12-09 10:27:01 +0000

    create sidebar and home

---
+++ src/app/template/sidebar/sidebar.component.ts
@@ -0,0 +1,15 @@
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css'],
+})
+export class SidebarComponent implements OnInit, AfterViewInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+  ngAfterViewInit(): void {
+
+  }
+}
